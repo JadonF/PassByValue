0c7b35fa3255d39821fe4912c3cbbfcdab848164
JadonF <36163291+JadonF@users.noreply.github.com>
2018-05-08 10:32:03 -0400

    Initial commit

---
+++ PassByValue/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PassByValue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAsk = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAsk
+            // 
+            this.lblAsk.AutoSize = true;
+            this.lblAsk.Location = new System.Drawing.Point(105, 71);
+            this.lblAsk.Name = "lblAsk";
+            this.lblAsk.Size = new System.Drawing.Size(150, 13);
+            this.lblAsk.TabIndex = 0;
+            this.lblAsk.Text = "Enter a temperature in celcius.";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(124, 116);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(137, 175);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Convert";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 329);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblAsk);
+            this.Name = "Form1";
+            this.Text = "Temperature Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAsk;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ PassByValue/Form1.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 8-May-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Temperature Converter
+ * This program converts °C to °F
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassByValue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ConvertToF(double degreesC)
+        {
+            //local variables
+            double degreesF;
+            string StringF;
+
+            //convert
+            degreesF = (double)(9) / (double)(5) * degreesC + 32;
+
+            StringF = Convert.ToString(degreesF);
+
+            //message
+            MessageBox.Show(txtInput.Text + "°C converted into Fahrenheit is " + StringF + "°F", "Conversion");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //local variables
+            double userDegrees;
+
+            //convert to double
+            userDegrees = Convert.ToDouble(txtInput.Text);
+
+            //call function
+            ConvertToF(userDegrees);
+        }
+    }
+}
